e6f53b5ee3ab333a92fdc394758c046483798944
Nnennia <106754370+Nnennia@users.noreply.github.com>
2023-01-07 04:21:06 +0100

    commit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
 -- Creates a MySql Server 8.0 with:
---  Database: hbnb_dev_db
---  User: hbnb_dev
---  Password:hbnb_dev_pwd
+--  Database: hbnb_test_db
+--  User: hbnb_test
+--  Password:hbnb_test_pwd
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-DROP USER IF EXISTS 'hbnb_dev'@'localhost';
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
