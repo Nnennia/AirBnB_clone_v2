6bcde77c7eb2f87f7d462596b9b391f54c7c96e2
Nnennia <106754370+Nnennia@users.noreply.github.com>
2023-01-07 04:16:27 +0100

    commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,21 +1,11 @@
--- create a MySQL server
--- DATABASSE : hbnb_dev_db
--- PASSWORD : hbnb_dev_pwd
--- SELECT privilege on the performance_schema
+-- Creates a MySql Server 8.0 with:
+--  Database: hbnb_dev_db
+--  User: hbnb_dev
+--  Password:hbnb_dev_pwd
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-
-GRANT ALL PRIVILEGES
-    ON 'hbnb_dev'.*
-    TO 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-
-GRANT SELECT
-    ON 'performance_schema'.*
-    TO 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,20 +1,11 @@
--- Creates a MySQL server with:
---   Database hbnb_test_db.
---   User hbnb_test
--- password hbnb_test_pwd in localhost.
---   Grants all privileges for hbnb_test on hbnb_test_db.
---   Grants SELECT privilege for hbnb_test on performance_schema.
+-- Creates a MySql Server 8.0 with:
+--  Database: hbnb_dev_db
+--  User: hbnb_dev
+--  Password:hbnb_dev_pwd
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
